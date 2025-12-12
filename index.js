caf0ec759d83d535feaf082409c4f55c6fdbd35a
Andrey Marques <andreymmarques6@gmail.com>
2025-12-11 22:14:08 -0300

    Created the function that displays the number, the add, subtract, multiply and divide functions and the operate function.

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-const add = function(n1, n2){
+let firstDigit = '';
+let operator = null;
+let secondDigit = '';
+
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".btn");
+
+function getDigit(){
+    digits.forEach(button => {
+        button.addEventListener('click', () => {
+            const clickedNumber = button.innerText;
+
+            if(display.innerText === '0'){
+                return display.innerText = clickedNumber;
+            } else {
+                return display.innerText += clickedNumber;
+            }
+        });
+    });
+}
+
+function add(n1, n2){
     return n1 + n2;
 }
 
-const subtract = function(n1, n2){
+function subtract(n1, n2){
     return n1 - n2;
 }
 
-const multiply = function(n1, n2){
+function multiply(n1, n2){
     return n1 * n2;
 }
 
-const divide = function(n1, n2){
+function divide(n1, n2){
     return n1 / n2;
 }
 
-let firstDigit;
-let secondDigit;
-let thirdDigit;
+function operate(operator, n1, n2){
+    n1 = Number(n1);
+    n2 = Number(n2);
 
-const operate = function(operator, n1, n2){
-    if (operator === '+') add(n1, n2);
-    if (operator === '-') subtract(n1, n2);
-    if (operator === '*') multiply(n1, n2);
-    if (operator === '/') divide(n1, n2);
+    switch(operator){
+        case '+':
+            return add(n1, n2);
+
+        case '-':
+            return subtract(n1, n2);
+
+        case '*':
+            return multiply(n1, n2);
+
+        case '/':
+            return divide(n1, n2);
+
+        default:
+            return null;
+    }
 }
